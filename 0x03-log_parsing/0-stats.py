ffa456707cbd8057fbcf8875f4224be951d00f07
kVAcodes <kadiriavictor@gmail.com>
2024-01-09 14:38:02 +0000

    removed infinite loop

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -9,26 +9,25 @@ if __name__ == '__main__':
         store = {
             'File size': 0
         }
-        while True:
-            for line in sys.stdin:
-                if count == 10:
-                    count = 0
-                    line_to_print = ''
-                    sorted_dict = list(sorted(store.items()))
-                    line_to_print += (
-                            'File size' + ': ' + str(sorted_dict[-1][1]) + '\n'
-                            )
-                    for key, value in sorted_dict[:-1]:
-                        line_to_print += key + ': ' + str(value) + '\n'
-                    print(line_to_print, end="")
-                # strip line
-                parsed = line.split()
-                store['File size'] += int(parsed[-1])
-                if parsed[-2] in store:
-                    store[parsed[-2]] += 1
-                else:
-                    store[parsed[-2]] = 1
-                count += 1
+        for line in sys.stdin:
+            if count == 10:
+                count = 0
+                line_to_print = ''
+                sorted_dict = list(sorted(store.items()))
+                line_to_print += (
+                        'File size' + ': ' + str(sorted_dict[-1][1]) + '\n'
+                        )
+                for key, value in sorted_dict[:-1]:
+                    line_to_print += key + ': ' + str(value) + '\n'
+                print(line_to_print, end="")
+            # strip line
+            parsed = line.split()
+            store['File size'] += int(parsed[-1])
+            if parsed[-2] in store:
+                store[parsed[-2]] += 1
+            else:
+                store[parsed[-2]] = 1
+            count += 1
     except KeyboardInterrupt:
         line_to_print = ''
         sorted_dict = list(sorted(store.items()))
